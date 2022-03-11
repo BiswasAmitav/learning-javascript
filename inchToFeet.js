045620bfabdd6157952ccd4ae8a3ff27ac7efba4
BiswasAmitav <biswasamit2648@gmail.com>
2022-03-11 23:02:58 +0530

    Converting inch to feet is successfully done

--- inchToFeet.js
+++ inchToFeet.js
@@ -1,3 +1,10 @@
-function inchToFeet(){
-    
+function inchToFeet(inch){
+    let dividor=12;
+    let result=inch/dividor;
+    return result;
 }
+let NaniFeet=inchToFeet(180);
+console.log('Nani Feet is: '+NaniFeet);
+
+let DadaFeet=inchToFeet(200);
+console.log('Dada feet is:' +DadaFeet);
