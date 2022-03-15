ea0ad7ac27f66740359dcf19793101ea73b61d3c
BiswasAmitav <biswasamit2648@gmail.com>
2022-03-15 18:23:12 +0530

    Here i've practiced some swaping method

---
+++ swap.js
@@ -0,0 +1,27 @@
+// var a=5;
+// var b=7;
+// console.log("before wap: a =", a, "b=", b);
+
+// var temp=a;
+// a=b;
+// b=temp;
+// console.log("After swap: a =", a, "b = ",b );
+
+
+
+// By another method
+var x=5;
+var y=10;
+console.log("Before swap : x = ",x,",", "y = ",y,".");
+x=x+y;
+y=x-y;
+x=x-y;
+console.log("After swap : x = ",x,",","y = ", y,".");
+
+
+// By another method which is used by Javascript
+var p=10;
+var q=20;
+console.log("Before swap : p = ",p,",","q = ", q,".");
+[p, q] = [q, p];
+console.log("After swap : p = ",p,",","q = ", q,".");
