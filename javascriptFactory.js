f8d91bcb09d2b574edb7f021e30d27b711d9e272
BiswasAmitav <biswasamit2648@gmail.com>
2022-03-17 18:08:40 +0530

    Second factory fucntion I've made

--- javascriptFactory.js
+++ javascriptFactory.js
@@ -8,5 +8,25 @@ function showProductInfo(productName,productId,productQuantity,productIsAvailabl
     }
 }
 
-console.log(showProductInfo('LotoShoe',23452,2,false));
-console.log(showProductInfo('amitav',190303105349,01,true));
+// console.log(showProductInfo('LotoShoe',23452,2,false));
+// console.log(showProductInfo('amitav',190303105349,01,true));
+
+
+function facultyMember(name,id,subject,allotedSection,numberOfDailyClass,isAllotedForLab,degree,universityName,age,maritalStatus){
+    // WHEN VALUE NAME AND PARAMETER NAME IS SAME NO NEED TO MENTIONED VALUE NAME FOR EVERY
+    return{
+        name,//:name,
+        id,//:id,
+        subject,//:subject,
+        allotedSection,//:allotedSection,
+        numberOfDailyClass,//:numberOfDailyClass,
+        isAllotedForLab,//:isAllotedForLab,
+        degree,//:degree,
+        universityName,//:universityName,
+        age,//:age,
+        maritalStatus//:maritalStatus
+    }
+}
+
+console.log(facultyMember('Yoothika Patel',349,'STQA','6b3',4,true,'Msc in SWE','Gujrat Technical University',41,true));
+console.log(facultyMember('Amitav Biswas',32432222222,'cse','6b4',5,false,'b.tech in cse','Parul University',22,false));
