ddd6884109a13033d0bdbc4331ca050409ae6dc5
BiswasAmitav <biswasamit2648@gmail.com>
2022-03-17 17:36:10 +0530

    This is first Programe for learning JavaScript Factory function

--- javascriptFactory.js
+++ javascriptFactory.js
@@ -1,1 +1,12 @@
-console.log("Hello amit ")
+console.log("Amitav is learning Javascript Factory....");
+function showProductInfo(productName,productId,productQuantity,productIsAvailable){
+    return{
+        productName: productName,
+        productId: productId,
+        productQuantity: productQuantity,
+        productIsAvailable: productIsAvailable
+    }
+}
+
+console.log(showProductInfo('LotoShoe',23452,2,false));
+console.log(showProductInfo('amitav',190303105349,01,true));
