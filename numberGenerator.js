1cd97acf01e36e38b4974d66d7fd2b33195873bc
BiswasAmitav <biswasamit2648@gmail.com>
2022-03-18 14:08:34 +0530

    Here I've learnt Javascript Math Function & How to generate the different number like LUDU

--- numberGenerator.js
+++ numberGenerator.js
@@ -1,7 +1,28 @@
 // console.log("Hello amit");
 
-var num=2.38293;
-var result=Math.floor(num);
-var result2=Math.ceil(num);
-console.log(result2)
-console.log(result);
+
+//SOME BASIC MATH FUNCTION....
+var num = 2.38293;
+var result = Math.floor(num);
+var result2 = Math.ceil(num);
+//console.log(result2)
+//console.log(result);
+
+var num2 = 3.85;
+result2 = Math.round(num2);
+//console.log(result2);
+
+
+var num3 = 2.499;
+var result3 = Math.round(num3);
+// console.log(result3);
+
+
+
+//THIS IS THE FUNCTION FOR GENERATING THE RANDOM NUMBER....
+
+for (var i = 0; i < 5; i++) {
+    var randomNumber = Math.random() * 10;
+    let output = Math.round(randomNumber);
+    console.log(output);
+}
