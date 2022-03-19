68b566a43626933c7fc4375119d0b6409b2fa163
BiswasAmitav <biswasamit2648@gmail.com>
2022-03-19 21:13:26 +0530

    In this programe i've learnt that how to find max number in given array

--- comparismNumber.js
+++ comparismNumber.js
@@ -1,10 +1,10 @@
-var business=450;
-var minitster=350;
-var sochib=550;
+// var business=450;
+// var minitster=350;
+// var sochib=550;
 
 
-var max=Math.max(business,minitster,sochib);
-console.log(max);
+// var max=Math.max(business,minitster,sochib);
+// console.log(max);
 
 // if(business>minitster){
    
@@ -23,3 +23,26 @@ console.log(max);
 //         console.log("Sochib is bigger");
 //     }
 // }
+
+//TO FIND THE MAX VALUE IN ARRAY
+
+var marks=[45,34,23,89,23,67,12,45,54,63,71,16,38];
+var max = marks[0];
+
+for(var i = 0; i < marks.length;i++){
+    var element=marks[i];
+    //TO PRINT ALL ELEMENTS ON ARRAY......
+
+    console.log(element)
+    if(element>max){
+        max=element;
+        // console.log(max);
+    }
+
+
+    // else{
+    //     console.log();
+    // }
+    
+}
+console.log("Highest value is : ",max);
