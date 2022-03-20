750af30d7bf25cf741d95c20f6516a1b1c89be28
BiswasAmitav <biswasamit2648@gmail.com>
2022-03-20 22:21:26 +0530

    final code for reverse

--- reverseArray.js
+++ reverseArray.js
@@ -1,9 +1,11 @@
-function resverseString(str){
+function reverseString(str){
     var reverse ="";
-    for(var i=0;i<str.length;i++){
+    for(var i=0; i<str.length;i++){
         var char = str[i];
         reverse=char+reverse;
     }
+    return reverse;
 }
 var statement="Amitav and Noushad vai";
-var forReverse=resverseString(statement);
+var forReverse=reverseString(statement);
+console.log(forReverse);
