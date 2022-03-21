63cdbf1ae65a9b95dd104e9b6d2dd78338975862
BiswasAmitav <biswasamit2648@gmail.com>
2022-03-21 22:31:00 +0530

    here i learnt how to pick multiple element by class name like array

--- JS Dom/firstDom.js
+++ JS Dom/firstDom.js
@@ -3,3 +3,17 @@ var font=document.getElementById('second');
 font.style.color='green';
 font.style.fontFamily='verdana';
 font.style.fontSize='2.5rem';
+
+
+//JS FOR DESIGNING PART AND HW TO PICK MULTIPLE ELEMENT.........
+var authors=document.getElementsByClassName('author');
+console.log(authors);
+for(var i=0;i<authors.length;i++){
+    var element=authors[i];
+    element.style.backgroundColor='purple';
+    element.style.color='white';
+    console.log(element);
+    console.log(element.innerHTML);
+    element.innerHTML="lekhoker dol -"+(i+1);
+    console.log(element.innerHTML);
+}
