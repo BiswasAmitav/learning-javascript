8aef82fc17342f43f12fca519884b5ae1cbe9bb4
BiswasAmitav <biswasamit2648@gmail.com>
2022-03-21 21:56:03 +0530

    in this programe i've learnt how to pick the element by dom

---
+++ JS Dom/firstDom.js
@@ -0,0 +1,5 @@
+document.getElementById('first').style.color='red';
+var font=document.getElementById('second');
+font.style.color='green';
+font.style.fontFamily='verdana';
+font.style.fontSize='2.5rem';
