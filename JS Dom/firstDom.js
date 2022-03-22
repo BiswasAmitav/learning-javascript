612e8fe8cf1e33fcb3fedbbf8eab6f3891d809a4
BiswasAmitav <biswasamit2648@gmail.com>
2022-03-22 18:02:27 +0530

    I learnt how pick element by query selector and how yo update insights text

--- JS Dom/firstDom.js
+++ JS Dom/firstDom.js
@@ -17,3 +17,12 @@ for(var i=0;i<authors.length;i++){
     element.innerHTML="lekhoker dol -"+(i+1);
     console.log(element.innerHTML);
 }
+
+
+//HERE I PRACTICE FOR QUERY SELECTOR....AND QUERY SELECTORALL
+
+
+var query=document.querySelector('.first h3');
+query.innerHTML="This is been changed by query selector";
+
+console.log(query);
