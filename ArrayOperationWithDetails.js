d6db617dc69016fb7fdc0f0537d2e31f0c5aa81c
BiswasAmitav <biswasamit2648@gmail.com>
2022-04-13 22:02:32 +0530

    Re-arrange to array by using imperative traverse

--- ArrayOperationWithDetails.js
+++ ArrayOperationWithDetails.js
@@ -3,7 +3,7 @@ console.log("hello amit");
 
 // REFERENCE FORM ARMY FULL STACK COURSE OF STACK LEARNER(VIDEO NO: 4-5);
 
-const numbers= [2,3,4,56,7,89];
+// const numbers= [2,3,4,56,7,89];
 //if we traverse this array by using for loop its called imperative traverse....
 
 // for(var i=0;i<numbers.length;i++){
@@ -61,10 +61,40 @@ const numbers= [2,3,4,56,7,89];
 
 
 // sum of array element upto specified array index;
-let sum=0;
-numbers.forEach(function(value,index){
-    if(index<3){
-        sum +=value;
+// let sum=0;
+// numbers.forEach(function(value,index){
+//     if(index<3){
+//         sum +=value;
+//     }
+// });
+// console.log(sum);
+
+
+
+
+// ============================================
+//ARRAY MANIPULATION LIKE (DELETE , UPDATE , RE-ARRANGE OF ARRAY AND MANY MORE)
+
+
+
+//COMPLICATED METHOD.....
+
+
+
+const arr=[1,2,3,null,false,4,5,'','test',6,7,8];
+
+let count=0;
+for (let i=0;i<arr.length;i++){
+    for(let j=i;j<arr.length-1;j++){
+        if(!arr[j]||typeof arr[j]==!'number'){
+            arr[j]=arr[j+1];
+            arr[j+1]=undefined;
+        }
     }
-});
-console.log(sum);
+    
+    if(arr[i]===undefined){
+        count++;
+    }
+}
+arr.length -=count;
+console.log(arr);
