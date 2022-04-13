11a145119e5563785a91832100dca045a051b899
BiswasAmitav <biswasamit2648@gmail.com>
2022-04-13 15:23:57 +0530

    here i've done that how to forEach function is work

--- ArrayOperationWithDetails.js
+++ ArrayOperationWithDetails.js
@@ -34,6 +34,37 @@ const numbers= [2,3,4,56,7,89];
 
 
 
-numbers.forEach(function(value, index, arr){
-    console.log(value,index,arr);
-})
+// numbers.forEach(function(value, index, arr){
+//     console.log(value,index,arr);
+// })
+
+
+
+//now we are able to do the operation what we supposed to be do in array for a particular problem.
+
+//even number in array
+// numbers.forEach(function(value){
+//     if(value%2==0){
+//         console.log(value);
+//     }
+// });
+
+
+
+//sum of array..
+// let sum=0;
+// numbers.forEach(function(v){
+//     sum +=v;
+// })
+// console.log(sum);
+
+
+
+// sum of array element upto specified array index;
+let sum=0;
+numbers.forEach(function(value,index){
+    if(index<3){
+        sum +=value;
+    }
+});
+console.log(sum);
