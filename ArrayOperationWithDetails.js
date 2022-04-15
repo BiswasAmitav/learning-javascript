8bc6962a404e2270c5ca654eeb2c305e85118803
BiswasAmitav <biswasamit2648@gmail.com>
2022-04-15 23:13:09 +0530

    here i used javascript filtered method

--- ArrayOperationWithDetails.js
+++ ArrayOperationWithDetails.js
@@ -81,20 +81,31 @@ console.log("hello amit");
 
 
 
-const arr=[1,2,3,null,false,4,5,'','test',6,7,8];
+// const arr=[1,2,3,null,false,4,5,'','test',6,7,8];
 
-let count=0;
-for (let i=0;i<arr.length;i++){
-    for(let j=i;j<arr.length-1;j++){
-        if(!arr[j]||typeof arr[j]==!'number'){
-            arr[j]=arr[j+1];
-            arr[j+1]=undefined;
-        }
-    }
+// let count=0;
+// for (let i=0;i<arr.length;i++){
+//     for(let j=i;j<arr.length-1;j++){
+//         if(!arr[j]||typeof arr[j]==!'number'){
+//             arr[j]=arr[j+1];
+//             arr[j+1]=undefined;
+//         }
+//     }
     
-    if(arr[i]===undefined){
-        count++;
-    }
-}
-arr.length -=count;
-console.log(arr);
+//     if(arr[i]===undefined){
+//         count++;
+//     }
+// }
+// arr.length -=count;
+// console.log(arr);
+
+
+
+
+// ======================
+
+// JAVASCRIPT FILTERED METHOD
+
+const arr=[1,2,3,null,false,4,5,'','test',6,7,8];
+const filteredArray= arr.filter((v) =>typeof v=='number');
+console.log(filteredArray);
