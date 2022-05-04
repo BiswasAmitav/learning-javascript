ffd996dde62ff20b226fba0bd3e9d6324accee5e
BiswasAmitav <biswasamit2648@gmail.com>
2022-05-04 15:03:06 +0530

    by this code we can controll our event listener that how many times it need to fire

--- JS Dom/eventlistener.js
+++ JS Dom/eventlistener.js
@@ -6,14 +6,81 @@ const chacha=document.querySelector('.chacha');
 const bhai=document.querySelector('.bhai');
 
 
-grand.addEventListener('click',e =>{
+
+// TO START FROM OUTSIDER......
+
+
+
+// grand.addEventListener('click',e =>{
+//     console.log('I am grand');
+// },
+// {capture:true}
+// );
+
+// chacha.addEventListener('click',e =>{
+//     console.log('I am chacha');
+// },
+// {capture:true}
+// );
+
+// bhai.addEventListener('click',e =>{
+//     console.log('I am bhai');
+// },
+// {capture:true}
+// );
+
+
+
+
+
+//TO PRINT AS LIKE BUBBLE....
+
+// grand.addEventListener('click',e =>{
+//     console.log('I am grand');
+// },
+// {capture:false}
+// );
+
+// chacha.addEventListener('click',e =>{
+//     console.log('I am chacha');
+// },
+// {capture:false}
+// );
+
+// bhai.addEventListener('click',e =>{
+//     console.log('I am bhai');
+// },
+// {capture:false}
+// );
+
+
+
+
+
+
+
+
+
+//BUT IN THIS METHOD ALL EVENT LISTENER IS CALLING AGAIN AND AGAIN
+/*  IF WE WANT EVENT LISTENER FOR A ONE TIM , THEN WE CAN PURIFY OUR 
+THIRD OBJECT....ONCE:1;
+once: true;
+*/
+
+grand.addEventListener('click',(e)=>{
     console.log('I am grand');
-});
+},
+{once:true}
+);
 
-chacha.addEventListener('click',e =>{
+chacha.addEventListener('click',(e)=>{
     console.log('I am chacha');
-});
+},
+{capture:true}
+);
 
-bhai.addEventListener('click',e =>{
+bhai.addEventListener('click',(e)=>{
     console.log('I am bhai');
-});
+},
+{capture:true}
+);
