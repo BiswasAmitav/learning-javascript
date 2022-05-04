620b3df987bd8a407e8b3f8b8691b693e11d3c23
BiswasAmitav <biswasamit2648@gmail.com>
2022-05-04 14:47:06 +0530

    in this code i learnt by bhai only all event is firing

---
+++ JS Dom/eventlistener.js
@@ -0,0 +1,19 @@
+
+const grand=document.querySelector('.grand');
+
+const chacha=document.querySelector('.chacha');
+
+const bhai=document.querySelector('.bhai');
+
+
+grand.addEventListener('click',e =>{
+    console.log('I am grand');
+});
+
+chacha.addEventListener('click',e =>{
+    console.log('I am chacha');
+});
+
+bhai.addEventListener('click',e =>{
+    console.log('I am bhai');
+});
