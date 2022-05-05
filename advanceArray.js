6f53fb35f5f55f415e363d123e93f9b939f631bb
BiswasAmitav <biswasamit2648@gmail.com>
2022-05-05 14:13:02 +0530

    by this tutorial with the help of reduce i learnt how to get single element in array

--- advanceArray.js
+++ advanceArray.js
@@ -26,7 +26,93 @@
 
 
 //SHORTCUT TECHNIQUE FOR ARRAY MAP================
-let names=['Ambika','Robin','amitav','Protap','Partha'];
+// let names=['Ambika','Robin','amitav','Protap','Partha'];
 
-let capatalizedArray=names.map((names)=>names.toUpperCase());
-console.log(capatalizedArray);
+// let capatalizedArray=names.map((names)=>names.toUpperCase());
+// console.log(capatalizedArray);
+
+
+
+
+
+//HOW TO EXTRACT SPECIFIC ARRAY ELEMENTS FROM LARGE ARRAY
+
+const characters=[
+    {
+        name:'amitav biswas',
+        height:'6.5',
+        mass:'56',
+        eye_color:'blue',
+        gender:'male'
+    },
+
+    {
+        name:'Protap biswas',
+        height:'6.5',
+        mass:'55',
+        eye_color:'black',
+        gender:'male'
+    },
+
+    {
+        name:'partha biswas',
+        height:'4.5',
+        mass:'35',
+        eye_color:'brown',
+        gender:'male'
+    },
+    {
+        name:'baba biswas',
+        height:'4.5',
+        mass:'35',
+        eye_color:'brown',
+        gender:'male'
+    }
+]
+
+//HERE WE WILL GET TOTAL ARRAY.............
+
+// let names=characters.map(function(characters){
+//     return characters.name;
+// });
+
+
+
+
+
+
+
+
+//BUT BY THIS REDUCE WE ACCESS SINGLE ELEMENT OF THEM 
+
+// let result= characters.reduce(function(acc,current){
+//     return acc+current.eye_color;
+// }, 1);
+
+// let result=characters.reduce((acc,current)=>{
+//     // return acc+current.name;
+// },{});
+
+
+
+
+
+
+// let eyeColor=characters.reduce((acc,curr)=>{
+//     let color=curr.eye_color;
+//     if(acc[color]){
+//         acc[color]=acc[color]+1;
+//     }
+//     else{
+//         acc[color]=1;
+//     }
+//     return acc;
+// },{});
+// console.log(eyeColor);;
+
+ 
+
+
+
+
+//FILTER HOW TO USE ARRAY FILTER 
