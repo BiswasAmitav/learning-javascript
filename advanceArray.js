bbe1939347cede2b51387fade94289e813eaf18e
BiswasAmitav <biswasamit2648@gmail.com>
2022-05-05 11:35:03 +0530

    in this code i learnt that how to write array map function with the help of array function in shortcut

--- advanceArray.js
+++ advanceArray.js
@@ -1,5 +1,32 @@
-let arr=[1,2,3];
 
-arr.map(function(num){
-    return num*2;
-})
+
+//ARRAY MAP=================
+
+// let arr=[1,2,3];
+
+
+// let newArr=arr.map(function(num){
+//     return num*2;
+// });
+
+
+// console.log(newArr);
+
+
+
+
+// let names=['Setu','Tamim','Sakib'];
+// let upperCase=names.map(function(names){
+//     return names.toUpperCase();
+// });
+
+// console.log(upperCase);
+
+
+
+
+//SHORTCUT TECHNIQUE FOR ARRAY MAP================
+let names=['Ambika','Robin','amitav','Protap','Partha'];
+
+let capatalizedArray=names.map((names)=>names.toUpperCase());
+console.log(capatalizedArray);
