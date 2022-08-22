a1f7d82e40e7e21cfe7d5272c73dc720859d4a21
BiswasAmitav <biswasamit2648@gmail.com>
2022-08-22 16:03:41 +0530

    hwo to use set time function

--- callStack.js
+++ callStack.js
@@ -2,20 +2,29 @@
 //and thats call execution constext....
 
 
-function add(a,b){
-    console.log("I am add..");
-    return a+b;
-}
+// function add(a,b){
+//     console.log("I am add..");
+//     return a+b;
+// }
 
-function multiply(x){
-    console.log("I am multiple..");
-    return x=add(10,20);
-}
+// function multiply(x){
+//     console.log("I am multiple..");
+//     return x=add(10,20);
+// }
 
-function divide(y){
-    console.log("i am divide ");
-    return multiply(2)/y;
-}
+// function divide(y){
+//     console.log("i am divide ");
+//     return multiply(2)/y;
+// }
 
-console.log(divide(5));
+// console.log(divide(5));
+
+setTimeout(()=>{
+    console.log("I should be first ... ");
+},0);//it will execute lapter bcz of it's asynchronus behave..
+
+console.log("i am first .");
+console.log("i am second .");
+
+
 
