1ec494aaaaf5d982601467dc62e390522d3f4f5a
BiswasAmitav <biswasamit2648@gmail.com>
2022-11-26 16:00:48 +0530

    Here i learnt how to iterate array element though for loop.

---
+++ functionInArrayIteration.js
@@ -0,0 +1,8 @@
+//Array iteration using forEach...
+
+var foods=["Cake","Ice Cream","Chocolate","Bread"];
+
+foods.forEach(function(item,index){
+    // console.log(item);
+    console.log(` For Index: ${index} , Item is ${item}`);
+});
