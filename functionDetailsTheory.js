74475fb80b95dc47035479125a18f0abe9946660
BiswasAmitav <biswasamit2648@gmail.com>
2022-11-26 15:15:24 +0530

    basic funciton types and declearation method i learnt

---
+++ functionDetailsTheory.js
@@ -0,0 +1,21 @@
+//Some Theory Part..
+/* We have two types of function in javascript. For instance 
+1. Built in function . 
+    like Prompt, alert, confirm. 
+2. User define function 
+    which we create for performing particular tasks in program.
+*/
+
+//#Technique to write a function..
+function saySomething(){
+    console.log("Hello ");
+    console.log("I'm learning JavaScript");
+}
+saySomething();
+
+//#Function Return type and Parameter . 
+
+
+
+
+
