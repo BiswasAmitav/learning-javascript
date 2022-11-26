dc803990207db7ac66525fb21dcebf506f35554c
BiswasAmitav <biswasamit2648@gmail.com>
2022-11-26 16:21:20 +0530

    here learnt about js object and methods as well

---
+++ objectsMethod.js
@@ -0,0 +1,8 @@
+let person={
+    firstName:"amitav",
+    lastName:"Biswas",
+    dob:"9-20-1999",
+    fullName:function(){//Methods of this objects.
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
