c0e696ef1e81697a025d06a3e074a99948feef57
BiswasAmitav <biswasamit2648@gmail.com>
2022-11-26 16:36:12 +0530

    math funciton and uses i learnt

---
+++ mathAndDateObject.js
@@ -0,0 +1,13 @@
+//Math Objects../
+//It is a built in OBjects..
+
+let val;
+val=Math.PI;
+val2=Math.E;
+let round=Math.round(3.6);
+let ceil=Math.ceil(4.2);
+console.log(val);
+console.log(val2);
+console.log(round);
+console.log(ceil);
+let floor=Math.floor(5.4);
