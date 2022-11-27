f66cd9f6e3fde53dcad1f5b138e2519766422946
BiswasAmitav <biswasamit2648@gmail.com>
2022-11-27 22:04:50 +0530

    this is law or wquation to find out the current age

---
+++ claculateAge.js
@@ -0,0 +1,9 @@
+//THIS IS VERY VERY IMPORTANT ....   
+   
+   // calculateAge(){
+    //     let birthday=new Date(this.dob);
+    //     let diff=date.now()-birthday.getTime();
+    //     let ageDate=new Date(diff);
+    //     return Math.abs(ageDate.getUTCFullYear()-1970);
+    // }
+
