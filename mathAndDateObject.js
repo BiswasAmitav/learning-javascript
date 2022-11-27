c8548b452a10ced2d8f2ba9a60e86581f27191d1
BiswasAmitav <biswasamit2648@gmail.com>
2022-11-27 11:23:29 +0530

    some math function i used

--- mathAndDateObject.js
+++ mathAndDateObject.js
@@ -11,3 +11,29 @@ console.log(val2);
 console.log(round);
 console.log(ceil);
 let floor=Math.floor(5.4);
+
+let sqrt=Math.sqrt(100);
+console.log(sqrt);
+
+let abs=Math.abs(-8);
+console.log(abs);
+
+let pow=Math.pow(100,3);
+console.log(pow);
+
+let min=Math.min(12,23,442,21,232,323,43,534);
+console.log(min);
+
+let max=Math.max(121,323,4,345,353,56,4235,352324643,644335345633,3435353453636673554345345464366);
+console.log(max);
+
+let random=Math.random();
+console.log(random);
+
+let random_100=Math.random()*6;
+console.log(Math.round(random_100));
+
+
+//Date and time object details ....
+let td=new Date();
+console.log(td.toString);
